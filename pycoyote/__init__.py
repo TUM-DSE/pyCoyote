20a3022f176b25923a077e8842b04fa8c99df83a
Benjamin Ramhorst <benjamin.ramhorst@inf.ethz.ch>
2025-10-29 15:28:06 +0100

    Set-up basic build proces to allow installation of pyCoyote via pip.

---
+++ pycoyote/__init__.py
@@ -0,0 +1,7 @@
+# Main __init__.py file, which allows pyCoyote to be imported as a Python library
+
+# NOTE: From the docs, this file must be placed in <package_name>, src/<package_name>, or python/<package_name> 
+# to be auto-discovered; in our case, <package_name> corresponds to pycoyote
+
+# Expose all the Python functions, classes etc.
+from ._pycoyote import *
